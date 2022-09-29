5ecce0147e43995ff70db79ead8d0b3ce1a1f0b9
Palusijf <palusijf@yahoo.fr>
2022-09-29 20:50:14 +0200

    Step 2 main.js change

--- Hypergram/task/src/main.js
+++ Hypergram/task/src/main.js
@@ -1,27 +1,26 @@
-let canvas = document.getElementById('canvas');
-const file_input = document.getElementById("file-input");
-canvas.width = 0;
-canvas.height = 0;
-const original_data = [];
+let originalPixels;
+const getCanvas = () => document.getElementById('canvas');
+const getCtx = () => getCanvas().getContext('2d');
 
-file_input.addEventListener("change", (e) => {
-    if (e.target.files) {
-        let file = e.target.files[0];
-        let reader = new FileReader();
+const drawImageOnload = image => (_) => {
+    const canvas = getCanvas();
+    const ctx = getCtx();
+    canvas.width = image.width;
+    canvas.height = image.height;
+    ctx.drawImage(image, 0, 0);
+    originalPixels = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+};
+
+const fileInput = document.getElementById('file-input');
+fileInput.addEventListener('change', (ev) => {
+    if (ev.target.files) {
+        const file = ev.target.files[0];
+        const reader = new FileReader();
         reader.readAsDataURL(file);
-        reader.onload = function (e) {
-            let image = new Image();
-            image.src = e.target.result;
-            image.onload = function (e) {
-                canvas.width = image.width;
-                canvas.height = image.height;
-                let ctx = canvas.getContext('2d');
-                ctx.drawImage(image, 0, 0);
-                let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                for (let i = 0; i < imageData.data.length; i++) {
-                    original_data[i] = imageData.data[i];
-                }
-            };
+        reader.onloadend = (e) => {
+            const image = new Image();
+            image.src = String(e.target.result);
+            image.onload = drawImageOnload(image);
         };
     }
 });
